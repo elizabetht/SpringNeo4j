54f33a0848d8c9e66273f5ff945ca2b2249200eb
ignacio.suay <ignacio.suay@gmail.com>
2016-12-22 16:47:48 +0100

    generate getters and setters for movie

--- src/main/java/com/ignaciosuay/domain/Movie.java
+++ src/main/java/com/ignaciosuay/domain/Movie.java
@@ -27,6 +27,79 @@ public class Movie {
     @Relationship(type = "RATED")
     List<Rating> ratings;
 
+    public Long getId() {
+        return id;
+    }
 
+    public void setId(Long id) {
+        this.id = id;
+    }
 
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Person getDirector() {
+        return director;
+    }
+
+    public void setDirector(Person director) {
+        this.director = director;
+    }
+
+    public Set<Person> getActors() {
+        return actors;
+    }
+
+    public void setActors(Set<Person> actors) {
+        this.actors = actors;
+    }
+
+    public List<Rating> getRatings() {
+        return ratings;
+    }
+
+    public void setRatings(List<Rating> ratings) {
+        this.ratings = ratings;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Movie movie = (Movie) o;
+
+        if (id != null ? !id.equals(movie.id) : movie.id != null) return false;
+        if (title != null ? !title.equals(movie.title) : movie.title != null) return false;
+        if (director != null ? !director.equals(movie.director) : movie.director != null) return false;
+        if (actors != null ? !actors.equals(movie.actors) : movie.actors != null) return false;
+        return ratings != null ? ratings.equals(movie.ratings) : movie.ratings == null;
+
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id != null ? id.hashCode() : 0;
+        result = 31 * result + (title != null ? title.hashCode() : 0);
+        result = 31 * result + (director != null ? director.hashCode() : 0);
+        result = 31 * result + (actors != null ? actors.hashCode() : 0);
+        result = 31 * result + (ratings != null ? ratings.hashCode() : 0);
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "Movie{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", director=" + director +
+                ", actors=" + actors +
+                ", ratings=" + ratings +
+                '}';
+    }
 }
