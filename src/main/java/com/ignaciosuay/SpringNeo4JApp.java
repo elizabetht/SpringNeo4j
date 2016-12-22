0f4ce7d2607d37ec4048d2e773b9312da0b7d43d
ignacio.suay <ignacio.suay@gmail.com>
2016-12-22 16:47:05 +0100

    remove unused imports

--- src/main/java/com/ignaciosuay/SpringNeo4JApp.java
+++ src/main/java/com/ignaciosuay/SpringNeo4JApp.java
@@ -1,13 +1,12 @@
 package com.ignaciosuay;
 
 import com.ignaciosuay.config.PersistenceContext;
-import org.junit.runner.RunWith;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 /**
  * Created by natxo on 17/12/16.
